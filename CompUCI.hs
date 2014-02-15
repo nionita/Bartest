35402fcfa7f71214b6dacb1d95173ead11645c74
Nicu Ionita <nicu@ionita.at>
2014-02-15 16:18:40 +0100

    Utility: Compare 2 UCI engines; tour.sh small changes

--- CompUCI.hs
+++ CompUCI.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Control.Concurrent.Async
 import Control.Exception
 import Data.List (isPrefixOf)
 import System.Environment (getArgs)
@@ -12,8 +13,19 @@ rin = "j:\\Barbarossa\\"
 prf = "j:\\Barbarossa\\dist\\build\\Barbarossa\\"
 
 main = do
-    (engine:_) <- getArgs
-    oneProc engine
+    (engine1:engine2:_) <- getArgs
+    r1 <- async $ oneProc engine1
+    r2 <- async $ oneProc engine2
+    (t1, n1) <- wait r1
+    (t2, n2) <- wait r2
+    printRes engine1 n1 t1
+    printRes engine2 n2 t2
+
+printRes eng n t = do
+    putStrLn $ "Engine " ++ eng ++ ": " ++ show n ++ " nodes, " ++ show t ++ " ms: "
+                 ++ show (round v) ++ " nodes/sec"
+    where v :: Double
+          v = fromIntegral n * 1000 / fromIntegral t
 
 oneProc :: String -> IO (Int, Int)
 oneProc engine = do
@@ -26,26 +38,25 @@ oneProc engine = do
         terminateProcess ph
         throwIO e
     putStrLn $ engine ++ ": done, with " ++ show r
-    -- terminateProcess ph	-- do we need this?
     return r
 
 lineUntil :: (String -> Bool) -> Handle -> IO String
 lineUntil p h = do
     l <- hGetLine h
-    putStrLn l
+    -- putStrLn l
     if p l then return l
            else lineUntil p h
 
-depth = 12	-- fix depth
-ttime = 1000000
+depth = 13	-- fix depth
+ttime = 480000
 
 runFen :: Handle -> Handle -> IO (Int, Int)
 runFen hi ho = do
     hPutStrLn hi "uci"
     lineUntil ("uciok" `isPrefixOf`) ho
     hPutStrLn hi $ "position startpos"	-- evtl with fen
-    -- hPutStrLn hi $ "go depth " ++ show depth
-    hPutStrLn hi $ "go wtime " ++ show ttime
+    hPutStrLn hi $ "go depth " ++ show depth
+    -- hPutStrLn hi $ "go movestogo 1 wtime " ++ show ttime
     (t, n) <- accumLines ho ("bestmove " `isPrefixOf`) getTimeNodes (1, 0)
     hPutStrLn hi "quit"
     return (t, n)
@@ -54,7 +65,7 @@ accumLines :: Handle -> (String -> Bool) -> (String -> a -> a) -> a -> IO a
 accumLines h p f = go
     where go a = do
              l <- hGetLine h
-             putStrLn l
+             -- putStrLn l
              if p l then return a
                     else go $! f l a
 
--- tour.sh
+++ tour.sh
@@ -1,4 +1,4 @@
-n=$(date +%Y%m%d)
+n=$(date +%Y%m%d%H)
 
 p=$1
 
@@ -11,8 +11,8 @@ exec >tour-$n.log 2>&1
 date
 
 nohup ./cutechess-cli.sh -concurrency 7 -draw movenumber=20 movecount=5 score=5 -resign movecount=5 score=800 -tournament round-robin -event TEST-$n -games 2 -rounds $p -pgnout TEST-$n.pgn -recover -each option.Hash=512 tc=60+1 arg=-l arg=5 \
-	-engine conf=addw \
 	-engine conf=exts \
-	-engine conf=clse \
-	-engine conf=addr \
+	-engine conf=addw \
+	-engine conf=cltv \
+	-engine conf=cltvf \
 	&
