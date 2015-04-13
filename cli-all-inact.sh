fa6c7865cde259b476a97baf47b35c4df7c4fa9b
Nicu Ionita <nicu@ionita.at>
2015-04-13 22:29:47 +0200

    Inactivate engines change
    The script cli-all-inact.sh moves now the engines from
    the inactive list (which is filled by cli-all-toina.sh)
    to the inactive directory

--- cli-all-inact.sh
+++ cli-all-inact.sh
@@ -2,18 +2,22 @@
 
 ahosts=$(./cli-all-hosts.sh R)
 
-elist=''
-for v in 0.3.0 0.2.0 0.1.0
-do
-	for e in $*
-	do
-		eng=Barbarossa-$v-$e
-		elist="$elist $eng"
-	done
-done
-
 for m in $ahosts
 do
 	echo Server $m
-	ssh $m 'cd ~/Engines; mkdir -p inactive; for e in '$elist';do if [ -x $e ]; then echo Engine $e; mv $e inactive; fi; done'
+	ssh $m '\
+		cd ~/Engines;\
+		for e in $(cat ~/Tour/.inactive.txt);\
+		do \
+			for v in 0.3.0 0.2.0 0.1.0; \
+			do \
+				eng=Barbarossa-$v-$e; \
+				if [ -e $eng ]; \
+				then \
+					echo $eng; \
+					mv $eng inactive; \
+				fi; \
+			done; \
+		done; \
+		cat /dev/null > ~/Tour/.inactive.txt'
 done
